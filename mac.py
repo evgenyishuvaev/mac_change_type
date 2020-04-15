84ba9c32b83c448886d608316eb112b6b09cd14b
Евгений Шуваев <e.shuvaev@telko.lan>
2020-04-15 08:35:23 +0300

    added pyperclip

--- mac.py
+++ mac.py
@@ -4,81 +4,103 @@
 
 # Переменная run созданная для работы цикла, после выполения условия и заключающей операции 
 # значение переменной меняется на False что останавливает цикл 
-# и запускает последнюю функцию вывода результата
+import pyperclip
 run = True
 
 
 while run :
-	addres = input('Введите mac-address: ')
-	
-	
+	addres = pyperclip.paste() #input('Введите mac-address: ')
+		
+	if len(addres) == 14 or len(addres) == 17 :
 
-	if '-' in addres :
+		if '-' in addres :
 
-		if input('Выберите формат:\n 1.BDCOM \n 2.The DUDE, QTECH, UTM\n --->') is '1' :
-			
-			# Dlink to BDCOM MAC-ADDRESS created
-			addres = addres.replace("-", "", 1)
-			addres = addres.replace("-", ".", 1)
-			addres = addres.replace("-", "", 1)
-			addres = addres.replace("-", ".", 1)
-			addres = addres.replace("-", "", 1)
-			
-			run = False
-		else :
-			# D-Link to The DUDE, QTECH, UTM created
-			addres = addres.replace("-", ":")
-			run = False
+			if input('Выберите формат:\n 1.BDCOM \n 2.The DUDE, QTECH, UTM\n --->') is '1' :
+				
+				# Dlink to BDCOM MAC-ADDRESS created
+				addres = addres.replace("-", "", 1)
+				addres = addres.replace("-", ".", 1)
+				addres = addres.replace("-", "", 1)
+				addres = addres.replace("-", ".", 1)
+				addres = addres.replace("-", "", 1)
+				
+				pyperclip.copy(addres)
+				print(addres)
+				
+				run = False
+			else :
+				# D-Link to The DUDE, QTECH, UTM created
+				addres = addres.replace("-", ":")
+				
+				pyperclip.copy(addres)
+				print(addres)
+				
+				run = False
 
-	elif ':' in addres :
-		if input('Выберите формат:\n 1.BDCOM \n 2.D-Link\n --->') is '1' :
+		elif ':' in addres :
+			if input('Выберите формат:\n 1.BDCOM \n 2.D-Link\n --->') is '1' :
 
-			# Dlink to BDCOM MAC-ADDRESS created
-			addres = addres.replace(":", "", 1)
-			addres = addres.replace(":", ".", 1)
-			addres = addres.replace(":", "", 1)
-			addres = addres.replace(":", ".", 1)
-			addres = addres.replace(":", "", 1)
+				# Dlink to BDCOM MAC-ADDRESS created
+				addres = addres.replace(":", "", 1)
+				addres = addres.replace(":", ".", 1)
+				addres = addres.replace(":", "", 1)
+				addres = addres.replace(":", ".", 1)
+				addres = addres.replace(":", "", 1)
 
-			run = False
+				pyperclip.copy(addres)
+				print(addres)
+				
+				run = False
 
-		else :
-			# The DUDE, QTECH, UTM to D-Link created
-			addres = addres.replace(":", "-")
+			else :
+				# The DUDE, QTECH, UTM to D-Link created
+				addres = addres.replace(":", "-")
+				
+				pyperclip.copy(addres)
+				print(addres)
+				
+				run = False
 
-			run = False
-	elif '.' in addres :
-		if input('Выберите формат:\n 1.The DUDE, QTECH, UTM \n 2.D-Link\n --->') is '1' :
-			# Следующие оперции разделят удалят из строки точку и разобьют строку на 6 частей
-			# и затем будут собраны с двоеточиями после каждых двух символом кроме последней пары
+		elif '.' in addres :
+			if input('Выберите формат:\n 1.The DUDE, QTECH, UTM \n 2.D-Link\n --->') is '1' :
+				# Следующие оперции разделят удалят из строки точку и разобьют строку на 6 частей
+				# и затем будут собраны с двоеточиями после каждых двух символом кроме последней пары
 
-			# BDCOM to The DUDE, QTECH, UTM created	
-			addres = addres.replace(".", "")
-			a = addres[0:2]
-			b = addres[2:4]
-			c = addres[4:6]
-			d = addres[6:8]
-			e = addres[8:10]
-			f = addres[10:12]
-			
+				# BDCOM to The DUDE, QTECH, UTM created	
+				addres = addres.replace(".", "")
+				a = addres[0:2]
+				b = addres[2:4]
+				c = addres[4:6]
+				d = addres[6:8]
+				e = addres[8:10]
+				f = addres[10:12]
+				
 
-			addres = a + ":" + b + ":" + c + ":" + d + ":" + e + ":" + f
+				addres = a + ":" + b + ":" + c + ":" + d + ":" + e + ":" + f
+				pyperclip.copy(addres)
+				print(addres)
+				run = False
 
-			run = False
-		else :
-			
-			#BDCOM to D-Link created 
-			addres = addres.replace(".", "")
-			a = addres[0:2]
-			b = addres[2:4]
-			c = addres[4:6]
-			d = addres[6:8]
-			e = addres[8:10]
-			f = addres[10:12]
+			else :
+				
+				#BDCOM to D-Link created 
+				addres = addres.replace(".", "")
+				a = addres[0:2]
+				b = addres[2:4]
+				c = addres[4:6]
+				d = addres[6:8]
+				e = addres[8:10]
+				f = addres[10:12]
 
-			addres = a + "-" + b + "-" + c + "-" + d + "-" + e + "-" + f
+				addres = a + "-" + b + "-" + c + "-" + d + "-" + e + "-" + f
 
-			run = False
+				pyperclip.copy(addres)
+				print(addres)
+
+				run = False
 
 
-print(addres)
+
+	else :
+		print("Incorrect mac-address")
+		run = False
